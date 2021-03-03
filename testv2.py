4e89350a04492de65cfbd54394b72f8866c4b11f
dactarie <dactarie@gmail.com>
2021-03-04 00:25:51 +0100

    maj ok tout fonctionne

--- testv2.py
+++ testv2.py
@@ -1,49 +1,69 @@
 import paramiko
 import shutil
-import datetime
 import os
 import time
 
-date = datetime.datetime.now()
-paramiko.util.log_to_file('/tmp/paramiko.log')
-host = 'cible1'
-log_to_export = "syslog,auth.log"
+host = os.listdir('srv/')
 
-src_folder = "/tmp/" + host
-remote_file = "/tmp/" + host + ".tar.gz"
-dst_file = "/tmp/" + host + ".tar.gz"
-dst_folder = "/home/guillaume/log/" + host + "/" + (date.strftime("%Y-%m-%d %H:%M:%S")) + "/"
-archive = "/tmp/" + host + ".tar.gz"
-port = 22
-username = 'root'
-
+#  Conf Paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(
-      paramiko.AutoAddPolicy())
-ssh.connect(hostname=host, port=port, username=username)
-sftp = ssh.open_sftp()
+    paramiko.AutoAddPolicy())
+port = 22
+username = 'root'
+paramiko.util.log_to_file('/tmp/paramiko.log')
+#  Fin de conf Paramiko
 
-print("## regoupement des fichiers dans " + remote_file)
-ssh.exec_command("mkdir " + src_folder)
-ssh.exec_command("cp /var/log/{" + log_to_export + "} " + src_folder)
-ssh.exec_command("cp /dev/null /var/log/{" + log_to_export + "} ")
 
-print("## creation de l'archive " + remote_file)
-ssh.exec_command("cd " + src_folder + " && tar -czvf " + archive + " .")
-print(archive)
-time.sleep(5)
+def regroupement():
+    print("- Regroupement des fichiers dans " + remote_file)
+    ssh.exec_command("mkdir " + src_folder)
+    with open("srv/" + host[i], "r") as f:
+        fichier = [line.strip() for line in f]
+        for g in range(len(fichier)):
+            ssh.exec_command("cp " + fichier[g] + " " + src_folder)
 
-print("## import du fichier archive ici " + remote_file + " a " + dst_file)
-sftp.get(remote_file, dst_file)
 
-print("## decompactage de larchive dans " + dst_folder)
-shutil.unpack_archive(dst_file, dst_folder)
+def creation_archive():
+    print("- Creation de l'archive : " + remote_file)
+    ssh.exec_command("cd " + src_folder + " && tar -czvf " + archive + " .")
+    time.sleep(3)
 
-print("## supression de l'archive distante + local " + remote_file + " " + dst_file)
-sftp.remove(remote_file)
-ssh.exec_command("rm -Rf " + src_folder)
-os.remove(dst_file)
 
-sftp.close()
-ssh.close()
-print("## Fin de la tache ##")
+def import_fichiers():
+    print("- Import du fichier archive ici : " + remote_file + " a " + dst_file)
+    sftp.get(remote_file, dst_file)
+
+
+def decompactage():
+    print("- Décompactage de larchive dans " + dst_folder)
+    shutil.unpack_archive(dst_file, dst_folder)
+
+
+def supression():
+    print("- Suppression de l'archive distante + local " + remote_file + " " + dst_file)
+    sftp.remove(remote_file)
+    ssh.exec_command("rm -Rf " + src_folder)
+    os.remove(dst_file)
+
+
+def close_ok():
+    sftp.close()
+    ssh.close()
+    print("##### Fin de la tache pour l'hote " + host[i] + " OK #####")
+
+
+for i in range(len(host)):
+    src_folder = "/tmp/" + host[i]
+    remote_file = "/tmp/" + host[i] + ".tar.gz"
+    dst_file = "/tmp/" + host[i] + ".tar.gz"
+    dst_folder = "/home/guillaume/log/" + host[i] + "/"
+    archive = "/tmp/" + host[i] + ".tar.gz"
+    ssh.connect(hostname=host[i], port=port, username=username)
+    sftp = ssh.open_sftp()
+    regroupement()
+    creation_archive()
+    import_fichiers()
+    decompactage()
+    supression()
+    close_ok()
