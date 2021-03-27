39e7a3c7396277a6f7568aaecde5876fd00123cd
dactarie <dactarie@gmail.com>
2021-03-27 23:15:41 +0100

    refactor(variables): variable, print
    - Variables cleaning for a better understanding
    - Cleaning all "print"
    - add comment

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ import time
 import datetime
 import threading
 
-#  Variables
+# Variables
 path_server = os.environ["HOME"] + "/log/"  # Where your want to import your file(s) (STEP 4)
 path_tmp = "/tmp/"  # Folder Temp On remote and local (STEP 4)
 log_paramiko = '/tmp/paramiko.log'  # Log Paramiko (STEP 4)
 login = 'root'  # Your user for SSH connect (STEP 4)
-
-
-# Fin des variables
+# End of variables
 
 
 class Launch:
@@ -24,14 +22,13 @@ class Launch:
         self.dst_archive = path_tmp + customer + ".tar.gz"
         self.dst_folder = path_server + customer + "/"
         self.archive = path_tmp + customer + ".tar.gz"
-        self.ssh = paramiko.SSHClient()
-        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.port = 22
-        self.username = login
-        self.paramiko_log = log_paramiko
-        paramiko.util.log_to_file(self.paramiko_log)
-        self.ssh.connect(hostname=self.host, port=self.port, username=self.username)
-        self.sftp = self.ssh.open_sftp()
+        self.ssh = paramiko.SSHClient()  # Only for Paramiko
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Only for Paramiko
+        self.port = 22  # Only for Paramiko
+        self.paramiko_log = log_paramiko  # Only for Paramiko
+        paramiko.util.log_to_file(self.paramiko_log)  # Only for Paramiko
+        self.ssh.connect(hostname=self.host, port=self.port, username=login)  # Only for Paramiko
+        self.sftp = self.ssh.open_sftp()  # Only for Paramiko
 
     def thread_function(self):
         execution = Launch(self.host)
@@ -72,13 +69,13 @@ class Launch:
             full_path_of_file = [line.strip() for line in list_of_file]
             for g in range(len(full_path_of_file)):
                 from os.path import basename
-                nom_de_fichier = basename(full_path_of_file[g])
-                taille = os.path.getsize(path_server + self.host + "/" + nom_de_fichier + "/" + nom_de_fichier + "-" +
+                name_of_file = basename(full_path_of_file[g])
+                size = os.path.getsize(path_server + self.host + "/" + name_of_file + "/" + name_of_file + "-" +
                                          (self.date.strftime("%F")))
-                if taille != 0:
+                if size != 0:
                     self.ssh.exec_command("> " + full_path_of_file[g])  # suppress content of file.
                 else:
-                    print("\x1b[41m" + "File : " + nom_de_fichier + " on " + self.host + " is empty !" + '\033[0m')
+                    print("\x1b[43m" + "File : " + name_of_file + " on " + self.host + " is empty !" + '\033[0m')
 
     def close_ok(self):  # This function close ssh and sftp.
         self.sftp.close()
@@ -87,12 +84,13 @@ class Launch:
 
     def scan_rapide(self):
         with open("customer/" + self.host, "r") as f:
-            dl = [line.strip() for line in f]
-            for g in range(len(dl)):
+            list_of_name_of_file = [line.strip() for line in f]
+            for name_of_file in range(len(list_of_name_of_file)):
                 from os.path import basename
-                fichier_a_scanner = \
-                    self.dst_folder + basename(dl[g]) + "/" + basename(dl[g]) + "-" + self.date.strftime("%F")
-                with open(fichier_a_scanner, "r") as toto:
+                files_to_scan = \
+                    self.dst_folder + basename(list_of_name_of_file[name_of_file]) + "/" \
+                    + basename(list_of_name_of_file[name_of_file]) + "-" + self.date.strftime("%F")
+                with open(files_to_scan, "r") as toto:
                     if "cible" in toto:
                         pass
                     else:
